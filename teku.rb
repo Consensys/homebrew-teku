8c1669dd98e2e2a1065c365b3a2b12a2a0493390
Adrian Sutton <adrian.sutton@consensys.net>
2020-12-07 19:48:11 +1000

    Update to 20.12.0

--- teku.rb
+++ teku.rb
@@ -1,11 +1,11 @@
 class Teku < Formula
   desc "Teku Ethereum 2 beacon chain client"
   homepage "https://github.com/consensys/teku"
-  url "https://bintray.com/consensys/pegasys-repo/download_file?file_path=teku-20.11.1.zip"
+  url "https://bintray.com/consensys/pegasys-repo/download_file?file_path=teku-20.12.0.zip"
   # update with: ./updateTeku.sh <new-version>
-  sha256 "07c547f79cf8502ebed0469440ea2c577e420dbb0a374a7ff2680fff38e34be4"
+  sha256 "95d1ad3f7de3e4b50da042c2fca9e91e60094f980230e84dbd66fc20e5ddb9ac"
 
-  depends_on "openjdk" => "11+"
+  depends_on :java => "11+"
 
   def install
     cp_r ".", "#{prefix}/dist"
