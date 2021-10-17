338c6f96be835dbdb3575372574ff9ce043c71f5
Adrian Sutton <adrian.sutton@consensys.net>
2021-10-18 08:47:46 +1000

    Upgrade to teku 21.10.0

--- teku.rb
+++ teku.rb
@@ -1,9 +1,9 @@
 class Teku < Formula
   desc "Teku Ethereum 2 beacon chain client"
   homepage "https://github.com/consensys/teku"
-  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/21.9.2/teku-21.9.2.zip"
+  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/21.10.0/teku-21.10.0.zip"
   # update with: ./updateTeku.sh <new-version>
-  sha256 "abc18835be9a645b4d7d3fff5cc9a589b4a0f5386c7de0c49f325f98eeafcf30"
+  sha256 "a213e81bc5c0c44b74b8e055d78f75edf8684ed7b701ff61d1d80800ee6bb350"
 
   depends_on "openjdk" => "11+"
 
