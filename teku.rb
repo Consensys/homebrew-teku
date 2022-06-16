4f8588f5915d12c1f0e388d36746c7fb45108045
Paul Harris <paul.harris@consensys.net>
2022-06-17 09:10:47 +1000

    22.6.0 release (#47)

--- teku.rb
+++ teku.rb
@@ -1,9 +1,9 @@
 class Teku < Formula
   desc "Teku Ethereum 2 beacon chain client"
   homepage "https://github.com/consensys/teku"
-  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/22.5.2/teku-22.5.2.zip"
+  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/22.6.0/teku-22.6.0.zip"
   # update with: ./updateTeku.sh <new-version>
-  sha256 "d482fbb574179cd14fb02e1198920a3c9de7c7ad313581d78ec7ff79b3e0cbca"
+  sha256 "b3a3df793504556879b2ff9825c54939ea2b82d9eac767b9bf8f21c2046535a7"
   head "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/develop/teku-develop.zip"
 
   depends_on "openjdk" => "11+"
