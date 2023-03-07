e0aed5c34d2cc65b34ac3192deec3d0876d7bd16
Mehdi AOUADI <mehdi.aouadi@gmail.com>
2023-03-07 13:34:30 +0100

    23.3.0 (#62)

--- teku.rb
+++ teku.rb
@@ -1,9 +1,9 @@
 class Teku < Formula
   desc "Teku Ethereum 2 beacon chain client"
   homepage "https://github.com/consensys/teku"
-  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/23.2.0/teku-23.2.0.zip"
+  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/23.3.0/teku-23.3.0.zip"
   # update with: ./updateTeku.sh <new-version>
-  sha256 "e41c43d0b68654983604b66736c64db8cbfe20bdb530b230e3bfe5a2280fb5b6"
+  sha256 "882fdf054cf47b4fe6a6e890036d9074f53f8c939f717efb8565394ffaf1826b"
   head "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/develop/teku-develop.zip"
 
   depends_on "openjdk" => "11+"
