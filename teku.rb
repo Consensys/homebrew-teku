7bb7583f524c570120fdd8a916ea6c90fa243a4b
Lucas Saldanha <lucas.saldanha@consensys.net>
2025-02-12 14:44:43 +1300

    Release 25.2.0 (#96)

--- teku.rb
+++ teku.rb
@@ -1,9 +1,9 @@
 class Teku < Formula
   desc "Teku Ethereum 2 beacon chain client"
   homepage "https://github.com/consensys/teku"
-  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/25.1.0/teku-25.1.0.zip"
+  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/25.2.0/teku-25.2.0.zip"
   # update with: ./updateTeku.sh <new-version>
-  sha256 "c4462aa066187c87e372360c83dde7e854ee91436c7b828e9584c9901f7f3d61"
+  sha256 "7ef4998d874b9f0118ef5b43f79ba42fef54ee328549cf87e38911ed4dc9f70f"
   head "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/develop/teku-develop.zip"
 
   depends_on "openjdk" => "21+"
