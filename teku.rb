500856980b561ba0b19b36380993a52aae25162b
Tekubot <tekubot@consensys.net>
2025-04-02 02:26:39 +0000

    Release 25.4.0

--- teku.rb
+++ teku.rb
@@ -1,9 +1,9 @@
 class Teku < Formula
   desc "Teku Ethereum 2 beacon chain client"
   homepage "https://github.com/consensys/teku"
-  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/25.3.0/teku-25.3.0.zip"
+  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/25.4.0/teku-25.4.0.zip"
   # update with: ./updateTeku.sh <new-version>
-  sha256 "5f256638d47be41be0fd6ac09c97970bbc70071e214e2075e1438a4807637d92"
+  sha256 "ba0887059434e65c9b89278be47cd8c1b094a66daa1dbb4dbe13b769110ac1c1"
   head "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/develop/teku-develop.zip"
 
   depends_on "openjdk" => "21+"
