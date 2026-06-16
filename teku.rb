3c1a5da2ccbdf8aecddad23f7c7ae602057e384c
Tekubot <tekubot@consensys.net>
2026-06-16 05:36:34 +0000

    Release 26.6.1

--- teku.rb
+++ teku.rb
@@ -1,9 +1,9 @@
 class Teku < Formula
   desc "Teku Ethereum 2 beacon chain client"
   homepage "https://github.com/consensys/teku"
-  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/26.6.0/teku-26.6.0.zip"
+  url "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/26.6.1/teku-26.6.1.zip"
   # update with: ./updateTeku.sh <new-version>
-  sha256 "58591aab5364230f94eea524e34497ba4a8d2f3fd1408137c3c772e5ea0b5dac"
+  sha256 "a8dce0202a9989ab85d735ef06d1eb663258497148c792c8d548735d53a59b81"
   head "https://artifacts.consensys.net/public/teku/raw/names/teku.zip/versions/develop/teku-develop.zip"
 
   depends_on "openjdk" => "21+"
